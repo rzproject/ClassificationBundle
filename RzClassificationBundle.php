52f6eef4b972bd4a73b6db39d22c21601b58004b
Mell Zamora <mellzamora@outlook.com>
2013-10-23 18:18:18 +0800

    added files

---
+++ RzClassificationBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rz\ClassificationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RzClassificationBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'SonataClassificationBundle';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
