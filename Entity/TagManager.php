2cf2d1872ec6517ef542adc3e243bcaf47526b55
mellzamora <mellzamora@outlook.com>
2016-05-20 02:54:33 +0800

    added query method

--- Entity/TagManager.php
+++ Entity/TagManager.php
@@ -6,5 +6,12 @@ use Sonata\ClassificationBundle\Entity\TagManager as BaseTagManager;
 
 class TagManager extends BaseTagManager
 {
+    public function geTagQueryForDatagrid($contexts) {
 
+        $qb = $this->getRepository()->createQueryBuilder('t');
+        $qb->select('t')
+            ->andWhere($qb->expr()->in('t.context', $contexts));
+
+        return $qb->getQuery();
+    }
 }
